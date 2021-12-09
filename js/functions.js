c39e8120a150d0401f433fbb1ca12cdc77f3cfd5
Kostya <kst.code@gmail.com>
2021-12-09 11:54:21 +0300

    add new functions comments

---
+++ js/functions.js
@@ -0,0 +1,32 @@
+"use strict";
+
+let num = 20;
+
+function showFirstMessage(text) {               //function declaration
+    console.log(text);
+    let num = 10;
+}
+
+showFirstMessage("hello world");
+console.log(num);
+
+// function calc(a, b) {                        //function declaration
+//     return a + b;
+// }
+
+// console.log(calc(1, 2));
+// console.log(calc(5, 2));
+// console.log(calc(10, 2));
+
+
+
+
+const logger = function(){                      //function expression
+    console.log("hello");
+};
+logger();
+
+
+const calc = (a, b) => {                        //arrow function
+    return a + b;
+}; 
--- js/script.js
+++ js/script.js
@@ -1,42 +1,42 @@
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", " ");
+// const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", " ");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     private: false
+// };
 
-// const a = prompt("Один из последний просмотренных фильмов?", " "),
-//     b = prompt("На сколько оцените его?", " "),
-//     c = prompt("Один из последний просмотренных фильмов?", " "),
-//     d = prompt("На сколько оцените его?", " ");
+// // const a = prompt("Один из последний просмотренных фильмов?", " "),
+// //     b = prompt("На сколько оцените его?", " "),
+// //     c = prompt("Один из последний просмотренных фильмов?", " "),
+// //     d = prompt("На сколько оцените его?", " ");
 
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
+// // personalMovieDB.movies[a] = b;
+// // personalMovieDB.movies[c] = d;
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt("Один из последний просмотренных фильмов?", " "),
-    b = prompt("На сколько оцените его?", " ");
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt("Один из последний просмотренных фильмов?", " "),
+//     b = prompt("На сколько оцените его?", " ");
 
-    if(a != null && b != null && a != ' ' && b != ' ' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
-}
+//     if(a != null && b != null && a != ' ' && b != ' ' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
 
-if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-}   else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-}   else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-}   else {
-    console.log("Ошибка");
-}
+// if (personalMovieDB.count < 10) {
+//     console.log("Просмотрено довольно мало фильмов");
+// }   else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log("Вы классический зритель");
+// }   else if (personalMovieDB.count >= 30) {
+//     console.log("Вы киноман");
+// }   else {
+//     console.log("Ошибка");
+// }
     
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
