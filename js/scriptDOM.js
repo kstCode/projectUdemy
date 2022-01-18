e050bfc0ce7fe21f4a6747e522534beb27e5cef7
Kostya <kst.code@gmail.com>
2022-01-18 11:45:48 +0300

    Work with selectors in DOM

---
+++ js/scriptDOM.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const box = document.getElementById('box');   //old
+console.log(box);
+
+const btns = document.getElementsByTagName('button');   //old
+console.log(btns[1]);
+
+const circles = document.getElementsByClassName('circle');  //old
+console.log(circles);
+
+
+const hearts = document.querySelectorAll('.heart');
+hearts.forEach((item, i) => {
+    console.log(item);
+    console.log(i);
+});
+
+// const oneHeart = document.querySelector('.heart');
+// console.log(oneHeart);
