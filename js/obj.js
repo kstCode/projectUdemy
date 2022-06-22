805fe92abebda6ef34f40aa2562d3d1c3a534d0f
kstCode <kst.code@gmail.com>
2022-06-22 13:39:25 +0300

    add new features

---
+++ js/obj.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    }
+};
+
+console.log(options.name);
