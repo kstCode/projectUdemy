0f002f6e8290bfb1e0332ba2a16fbc7fdf7b6dac
kstCode <kst.code@gmail.com>
2022-09-23 19:20:15 +0300

    feat: add promise.js file

---
+++ js/promise.js
@@ -0,0 +1,52 @@
+// 'use strict';
+
+// console.log('Запрос данных...');
+
+
+
+
+// const req = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log('Подготовка данных...');
+    
+//         const product = {
+//             name: 'TV',
+//             price: 2000
+//         };
+        
+//         resolve(product);
+//     }, 2000);
+// });
+
+// req.then((data) => {
+//     return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         data.status = 'order';
+//         resolve(data);
+        // reject();
+//     }, 2000);
+//     });
+// }).then((data) => {
+//     data.modify = 'true';
+//     return data;
+// }).then((data) => {
+//     console.log(data);
+// }).catch(() => {
+//     console.error('ERROOOOOOR');
+// }).finally(() => {
+//     console.log('Finally');
+// }); 
+
+const test = time => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, time);
+    });
+};
+
+// test(1000).then(() => console.log('1000ms'));
+// test(2000).then(() => console.log('2000ms'));
+
+// Promise.all([test(1000), test(2000)]).then(() => console.log('all complete'));
+Promise.race([test(1000), test(2000)]).then(() => console.log('1st complete'));
